66330647b1fd3cac22a3342e9fdae0796d75c936
Josh Robbs <josh@joshrobbs.com>
2024-10-14 11:05:57 -0400

    Add composer.json and plugin files for WPMB Custom Bricks Elements

---
+++ wpmb-custom-bricks-elements.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Plugin Name: WPMB Custom Bricks Elements
+ * Plugin URI: https://wpmasterbuilder.com
+ * Description: Custom elements for Bricks Builder (WordPress plugin).
+ * Version: 1.0.0
+ * Author: Josh Robbs
+ * Author URI: https://wpmasterbuilder.com
+ * License: GPL2
+ *
+ * @package WPMB_Custom_Bricks_Elements
+ */
+
+namespace WPMB_Custom_Bricks_Elements;
+
+use WPMB_Custom_Bricks_Elements\src\Rank_Math_Breadcrumbs;
+
+defined( 'ABSPATH' ) || exit;
+
+define( 'WPMB_CUSTOM_BRICKS_ELEMENTS_FILE', __FILE__ );
+define( 'WPMB_CUSTOM_BRICKS_ELEMENTS_PATH', plugin_dir_path( WPMB_CUSTOM_BRICKS_ELEMENTS_FILE ) );
+define( 'WPMB_CUSTOM_BRICKS_ELEMENTS_URL', plugin_dir_url( WPMB_CUSTOM_BRICKS_ELEMENTS_FILE ) );
+
+add_action( 'init', __NAMESPACE__ . '\init', 11 );
+
+/**
+ * Initialize the plugin.
+ *
+ * @return void
+ */
+function init() {
+	new Rank_Math_Breadcrumbs();
+}
